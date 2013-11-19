88e9623f0f62af7cde446d349288a58e59e8d8ca
Richard Walton <walton909@gmail.com>
2013-11-19 17:48:32 +0000

    Update Stage.hx
    Completed public and private functions.  Added types when possible.

--- createjs/easeljs/Stage.hx
+++ createjs/easeljs/Stage.hx
@@ -1,29 +1,41 @@
 package createjs.easeljs;
+
+import js.html.Element;
 import js.HTMLCanvasElement;
 
 @:native("createjs.Stage")
 extern class Stage extends Container {
 
-	public function new(canvas:Dynamic):Void;
-	public function clear():Void;
-	public function enableDOMEvents(?enable:Bool = true):Void;
-	public function enableMouseOver(?frequency:Float = 20):Void;
-	public function toDataURL(backgroundColor:String, mimeType:String):String;
-	public function update():Void;
-
+	@:overload(function(elementId:String):Void{})
+	public function new(canvas:HTMLCanvasElement):Void;
+	
 	public var autoClear:Bool;
-	public var canvas:Dynamic;
-	public var handleEvent:Dynamic;
-	public var mouseInBounds:Bool;
-	public var mouseMoveOutside:Bool;
+	public var canvas:HTMLCanvasElement;
 	public var mouseX:Float;
 	public var mouseY:Float;
-	public var nextStage:Stage;
-	//public var snapToPixelEnabled:Bool;
-	//public var tick:Dynamic;
+	public var mouseInBounds:Bool;
 	public var tickOnUpdate:Bool;
-
-	public var onMouseDown:Dynamic;
-	public var onMouseMove:Dynamic;
-	public var onMouseUp:Dynamic;
+	public var mouseMoveOutside:Bool;
+	public var nextStage:Stage;
+	
+	public function update():Void;
+	public function clear():Void;
+	public function toDataURL(backgroundColor:String, mimeType:String):String;
+	public function enableMouseOver(?frequency:Float = 20):Void;
+	public function enableDOMEvents(?enable:Bool = true):Void;
+	override public function clone():Stage;
+	override public function toString():String;
+	
+	function _getElementRect(element:Element): { top:Float, left:Float, bottom:Float, right:Float };
+	function _getPointerData(id:Int): { x:Float, y:Float };
+	function _handleMouseMove(e:MouseEvent):Void;
+	function _handlePointerMove(id:Int, e:MouseEvent, pageX:Float, pageY:Float):Void;
+	function _updatePointerPosition(id:Int, e:Event, pageX:Float, pageY:Float):Void;
+	function _handleMouseUp(e:MouseEvent):Void;
+	function _handlePointerUp(id:Int, e:Event, clear:Bool):Void;
+	function _handleMouseDown(e:MouseEvent):Void;
+	function _handlePointerDown(id:Int, e:Event, clear:Bool):Void;
+	function _testMouseOver(clear:Bool):Void;
+	function _handleDoubleClick(e:MouseEvent):Void;
+	function _dispatchMouseEvent(target:DisplayObject, type:String, bubbles:Bool, pointerId:Int, o:Dynamic, nativeEvent:MouseEvent):Void;
 }
