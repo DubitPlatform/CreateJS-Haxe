55e4fc3e3d1477cf78c837c18e4ec58691d7b422
andykay <yakydna@gmail.com>
2013-12-03 14:47:50 +0000

    Update ColorMatrix.hx

--- createjs/easeljs/ColorMatrix.hx
+++ createjs/easeljs/ColorMatrix.hx
@@ -1,7 +1,7 @@
 package createjs.easeljs;
 
 @:native("createjs.ColorMatrix")
-extern class ColorMatrix extends Array{
+extern class ColorMatrix{
 
 	public function new(brightness:Float, contrast:Float, saturation:Float, hue:Float):Void;
 	public function adjustBrightness(value:Float):ColorMatrix;
@@ -10,10 +10,10 @@ extern class ColorMatrix extends Array{
 	public function adjustHue(value:Float):ColorMatrix;
 	public function adjustSaturation(value:Float):ColorMatrix;
 	public function clone():ColorMatrix;
-	public function concat(matrix:Array):ColorMatrix;
-	public function copyMatrix(matrix:Array):ColorMatrix;
+	public function concat(matrix:Array<Float>):ColorMatrix;
+	public function copyMatrix(matrix:Array<Float>):ColorMatrix;
 	public function reset():ColorMatrix;
-	public function toArray():Array;
+	public function toArray():Array<Float>;
 
 	public static var DELTA_INDEX:Array<Float>;
 	public static var IDENTITY_MATRIX:Array<Int>;
